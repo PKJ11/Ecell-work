3244d884c90549ad9405ec567fa58808278b505f
Yash3456 <goyalyash311@gmail.com>
2023-12-19 22:45:12 +0530

    minor update

--- src/pages/ContentMarketing/ContentMarketing.jsx
+++ src/pages/ContentMarketing/ContentMarketing.jsx
@@ -5,7 +5,7 @@ import Calender from '../../components/Calender'
 
 const ContentMarketing = () => {
   return (
-    <div className='AppGlass'>
+    <div className='AppGlass'  style={{gridTemplateColumns:"11rem auto 0rem"}}>
       <Sidebar/>
       <MainDash heading= "Content Mrketing"/>
       <Calender/>
